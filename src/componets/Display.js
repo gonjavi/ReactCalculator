bdc52d93f26f8ad45548ff2e9f90f5f9adbee288
gonjavi <javiercaliescali@gmail.com>
2020-04-08 17:31:05 -0500

    constructor app.js

--- src/componets/Display.js
+++ src/componets/Display.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const displayStyle = {
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  border: '2px solid black',
-  justifyContent: 'flex-end',
-  background: '#848593',
-  height: 100,
-  color: 'white',
-  fontWeight: 'bold',
-  textAlign: 'right',
-  padding: 4,
+const DisStyle = {
+
 };
--- src/componets/app.js
+++ src/componets/app.js
@@ -3,7 +3,9 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+  }
   
   render() {
     return(

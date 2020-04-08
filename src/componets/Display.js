c4279b4d573bd1d3c45306c7dfc74ea4d47a28c1
gonjavi <javiercaliescali@gmail.com>
2020-04-08 17:30:53 -0500

    constructor app.js

---
+++ src/componets/Display.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const displayStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  border: '2px solid black',
+  justifyContent: 'flex-end',
+  background: '#848593',
+  height: 100,
+  color: 'white',
+  fontWeight: 'bold',
+  textAlign: 'right',
+  padding: 4,
+};
--- src/componets/app.js
+++ src/componets/app.js
@@ -15,3 +15,5 @@ class App extends React.Component {
     );
   }
 }
+
+export default App;

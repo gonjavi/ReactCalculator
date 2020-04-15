a6ce6faa82de1d6f0f3c5c7ff4f4e1ba34c2fc31
gonjavi <javiercaliescali@gmail.com>
2020-04-15 17:23:55 -0500

    Display

--- src/componets/Display.js
+++ src/componets/Display.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { lightblue } from 'color-name';
 
-export default  Display  {
-
-
+const style = {
+  background: 'lightblue',
 };
+
+const Display = props => {
+  const { result } = props;
+
+  return (
+    <div style={style}>
+      {result}
+    </div>
+  );
+};
+
+export default Display;

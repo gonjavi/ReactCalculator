852f08e12a84754072f8bad4da9cb79f406b1b80
gonjavi <javiercaliescali@gmail.com>
2020-04-16 14:25:17 -0500

    display

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { lightblue } from 'color-name';
 
 const style = {
-  
+  display: 'flex',
+  flexDirection: 'row',
+  width: 500,
+  marginLeft: 200,
+  justifyContent: 'flex-end',
   backgroundColor: 'lightblue',
 };
 

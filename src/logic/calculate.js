bc95ff27a4916f64a7c42f00e553123812803554
gonjavi <javiercaliescali@gmail.com>
2020-04-27 14:21:30 -0500

    implement calculate.js

---
+++ src/logic/calculate.js
@@ -0,0 +1,13 @@
+
+const calculate = buttonName => {
+  let total;
+  let next;
+  let operation;
+
+  if (buttonName === '+/-') {
+    total *= -1;
+    next *= -1;
+  }
+};
+
+export default calculate;

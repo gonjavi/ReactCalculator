90db5d8d85d561e0a54a5631595f0106c9959c8c
gonjavi <javiercaliescali@gmail.com>
2020-04-28 12:57:43 -0500

    complement calculate

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
+Calculate({ total, next, operation }, buttonName);
+
 function App() {
   return (
     <div className="App">
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,27 +1,26 @@
 import Operate from './operate';
 
 const Calculate = (data, buttonName) => {
- let { total, next, operation } = data;
+  let { total, next, operation } = data;
 
   if (buttonName === '+/-') {
     total *= -1;
     next *= -1;
   } else if (buttonName === 'AC') {
-
+    total = 0;
+    next = 0;
+    operation = 0;
   } else if (buttonName === '%') {
-    
+    total = Operate(one, two, '%');
   } else if (buttonName === '÷') {
-    total = Operate();
+    total = Operate(one, two, '÷');
   } else if (buttonName === 'X') {
-    
+    total = Operate(one, two, 'X');
   } else if (buttonName === '-') {
-    
+    total = Operate(one, two, '-');
   } else if (buttonName === '+') {
-    
-  } else if (buttonName === '=') {
-    
+    total = Operate(one, two, '+');
   }
-
   return { total, next, operation };
 };
 

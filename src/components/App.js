7ef8d413d0bed89b066a54b681102b957af54dfc
gonjavi <javiercaliescali@gmail.com>
2020-04-29 11:53:51 -0500

    pass handclick as a clickhandler

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Display result={total || next} />
-        <ButtonPanel />
+        <ButtonPanel onClick={this.handleClick} />
       </div>
     );
   }

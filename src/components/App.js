d747873ea320e592ecc2ffd5c7bacb05902207fb
gonjavi <javiercaliescali@gmail.com>
2020-04-29 11:38:58 -0500

    implement handle click

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,37 @@ import React from 'react';
 import './App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  HandleClick(buttonName) {
+    const data = Calculate(this.state, buttonName);
+    const { total, next, operation } = data;
+
+    this.setState({
+      total,
+      next,
+      operation,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
